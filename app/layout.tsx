bbdf8f180e5087dcb2797acf6adf6925dcffd8c3
makeaninvite-ux <makeaninvite@gmail.com>
2026-04-19 14:02:20 -0700

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Divorce Bill Review",
+  description: "Know what to question"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
